eab0493d8e76ef9ce02cedacabd4f4a7b0de3e90
moddedmcplayer <moddedmcplayer@gmail.com>
2022-04-07 15:15:47 +0200

    funni

---
+++ Config.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using Exiled.API.Interfaces;
+using minigun.Items;
+
+namespace minigun
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("The amount of bullets to spawn per shot")]
+        public int bullets { get; set; } = 5;
+
+        [Description("The delay between bullets")]
+        public float delay { get; set; } = 0.1f;
+
+        [Description("The damage of a single bullet")]
+        public float damage = 10f;
+        
+        public Items.minigun Minigun { get; set; } = new Items.minigun { Type = ItemType.GunLogicer };
+
+        public Items.sniper sniper { get; set; } = new sniper {Type = ItemType.GunRevolver };
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Exiled.API.Features;
+using Exiled.CustomItems.API;
+using MEC;
+
+namespace minigun
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Author { get; } = "moddedmcplayer";
+        public override string Name { get; } = "minigun";
+        public override Version Version { get; } = new Version(0, 0, 1);
+        public override Version RequiredExiledVersion { get; } = new Version(5, 0, 0);
+
+        public static Plugin Instance { get; private set; }
+        
+        public override void OnEnabled()
+        {
+            Instance = this;
+            Config.Minigun.Register();
+            Config.sniper.Register();
+            base.OnEnabled();
+        }
+        
+        public override void OnDisabled()
+        {
+            Config.Minigun.Unregister();
+            Config.sniper.Unregister();
+            Instance = null;
+            base.OnDisabled();
+        }
+    }
+}
